f30fcadaf7e903546239b0c6ed935c9a72716df7
Louise Pieri <cpieri@student.42.fr>
2020-01-15 14:55:37 +0100

    [Update]: Change the main custom to the main in the subject

--- d01/ex06/main.cpp
+++ d01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 10:25:53 by cpieri            #+#    #+#             */
-/*   Updated: 2020/01/15 11:47:28 by cpieri           ###   ########.fr       */
+/*   Updated: 2020/01/15 14:54:38 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,20 @@
 #include "HumanA.hpp"
 
 int		main(void) {
-	Weapon club = Weapon("crude spiked club");
-	HumanA bob("Bob", club);
-	bob.attack();
-	club.setType("some other type of club");
-	bob.attack();
-
-	Weapon club2 = Weapon("crude spiked club");
-	HumanB jim("Jim");
-	jim.setWeapon(club2);
-	jim.attack();
-	club2.setType("some other type of club");
-	jim.attack();
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
 	return (0);
 }
