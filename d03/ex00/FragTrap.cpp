7c98c39f7b1cb9ac3fb7d9aea868a4046c0716cb
Louise Pieri <cpieri@student.42.fr>
2020-01-16 15:53:26 +0100

    [WIP]: the ex00 of the d03 wip

--- d03/ex00/FragTrap.cpp
+++ d03/ex00/FragTrap.cpp
@@ -6,13 +6,13 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 13:37:54 by cpieri            #+#    #+#             */
-/*   Updated: 2020/01/16 13:47:24 by cpieri           ###   ########.fr       */
+/*   Updated: 2020/01/16 15:38:30 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(void) : _name("default"),
+FragTrap::FragTrap(void) : _name("ClapTrap"),
 _hit_points(100),
 _max_hit_points(100),
 _energy_points(100),
@@ -21,6 +21,7 @@ _level(1),
 _melee_attack_damage(30),
 _ranged_attack_damage(20),
 _armor_damage_reduction(5) {
+	std::cout << "I'm the butt of a pandocorn." << std::endl;
 }
 
 FragTrap::FragTrap(std::string name) : _name(name),
@@ -32,13 +33,16 @@ _level(1),
 _melee_attack_damage(30),
 _ranged_attack_damage(20),
 _armor_damage_reduction(5) {
+	std::cout << "<" << name << "> I'm the butt of a pandocorn." << std::endl;
 }
 
 FragTrap::FragTrap(FragTrap const & src) {
+	std::cout << "Et pan !" << std::endl;
 	*this = src;
 }
 
 FragTrap::~FragTrap(void) {
+	std::cout << "I am fire, I am death." << std::endl;
 }
 
 std::string		FragTrap::getName(void) const {
@@ -46,17 +50,17 @@ std::string		FragTrap::getName(void) const {
 }
 
 void			FragTrap::rangedAttack(std::string const & target) {
-	std::cout << std::endl;
+	std::cout << "FR4G-TP <" << this->_name << "> attacks <" << target << "> at range, causing " << this->_ranged_attack_damage << " points of damage !" << std::endl;
 }
 
 void			FragTrap::meleeAttack(std::string const & target) {
-	std::cout << std::endl;
+	std::cout << "FR4G-TP <" << this->_name << "> attacks <" << target << "> at melee, causing " << this->_melee_attack_damage << " points of damage !" << std::endl;
 }
 
 void			FragTrap::takeDamage(unsigned int amount) {
-	std::cout << std::endl;
+	std::cout << "FR4G-TP <" << this->_name << "> took " << amount << " points of damage !" << std::endl;
 }
 
 void			FragTrap::beRepaired(unsigned int amount) {
-	std::cout << std::endl;
+	std::cout << "FR4G-TP <" << this->_name << "> repairs himself by " << amount << " points !" << std::endl;
 }
