5a8ea5d989102c23daa1239a413e6e605299276c
Louise Pieri <cpieri@student.42.fr>
2020-01-16 09:19:04 +0100

    [Exercice]: Push the ex01 of the D02

--- d02/ex01/Fixed.cpp
+++ d02/ex01/Fixed.cpp
@@ -6,11 +6,12 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 18:49:40 by cpieri            #+#    #+#             */
-/*   Updated: 2020/01/15 20:02:56 by cpieri           ###   ########.fr       */
+/*   Updated: 2020/01/16 09:17:36 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <cmath>
 
 int const	Fixed::_bits = 8;
 
@@ -18,12 +19,14 @@ Fixed::Fixed(void) : _value(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(int const nb) : _value(nb) {
+Fixed::Fixed(int const nb) {
 	std::cout << "Int constructor called" << std::endl;
+	this->_value = nb << Fixed::_bits;
 }
 
-Fixed::Fixed(float const fnb) : _value(fnb) {
+Fixed::Fixed(float const fnb) {
 	std::cout << "Float constructor called" << std::endl;
+	this->_value = roundf(fnb * (1 << Fixed::_bits));
 }
 
 Fixed::Fixed(Fixed const & src) {
@@ -42,16 +45,22 @@ Fixed&		Fixed::operator=(const Fixed& rhs) {
 }
 
 int			Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_value);
 }
 
 void		Fixed::setRawBits(int const raw) {
-	std::cout << "setRawBits member function called" << std::endl;
 	this->_value = raw;
 }
 
+float		Fixed::toFloat(void) const {
+	return ((double)this->_value / (double)(1 << Fixed::_bits));
+}
+
+int			Fixed::toInt(void) const {
+	return (this->_value >> Fixed::_bits);
+}
+
 std::ostream &			operator<<(std::ostream & o, Fixed const & i) {
-	o << i.getRawBits();
+	o << i.toFloat();
 	return (o);
 }
--- d02/ex01/main.cpp
+++ d02/ex01/main.cpp
@@ -6,20 +6,26 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 19:02:56 by cpieri            #+#    #+#             */
-/*   Updated: 2020/01/15 19:04:01 by cpieri           ###   ########.fr       */
+/*   Updated: 2020/01/16 09:06:45 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int		main(void) {
-	Fixed		a;
-	Fixed		b(a);
-	Fixed		c;
+	Fixed			a;
+	Fixed const		b(10);
+	Fixed const		c(42.42f);
+	Fixed const		d(b);
 
-	c = b;
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	a = Fixed(1234.4321f);
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return (0);
 }
